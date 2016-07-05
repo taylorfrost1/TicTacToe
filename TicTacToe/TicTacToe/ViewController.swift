4a374abdb0bcbda59e33c033a52cf41fcd6c44d7
Taylor Frost <taylorhamblinfrost@gmail.com>
2016-07-05 16:46:56 -0600

    Done up to point of converting button's to strings

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -9,14 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var ticTacArray: [String] = ["X", "O", "X", "O", "X", "X", "X", "O", "X"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+//    self.buttonOutletOne = self.ticTacArray[0]
+        
+        
     }
-
-
-
-
+   
+    
+    @IBOutlet weak var buttonOutletZero: UIButton!
+    @IBOutlet weak var buttonOutletOne: UIButton!
+    @IBOutlet weak var buttonOutletTwo: UIButton!
+    @IBOutlet weak var buttonOutletThree: UIButton!
+    @IBOutlet weak var buttonOutletFour: UIButton!
+    @IBOutlet weak var buttonOutletFive: UIButton!
+    @IBOutlet weak var buttonOutletSix: UIButton!
+    @IBOutlet weak var buttonOutletSeven: UIButton!
+    @IBOutlet weak var buttonOutletEight: UIButton!
+    
 }
 
