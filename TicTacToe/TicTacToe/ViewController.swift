2b6edfe62e87131ffcc3b018d5d0c513f26455ee
Taylor Frost <taylorhamblinfrost@gmail.com>
2016-07-05 22:13:27 -0600

    Done with the assignment

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -14,11 +14,20 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//    self.buttonOutletOne = self.ticTacArray[0]
-        
+        self.buttonOutletZero.setImage(imageX, forState: .Normal)
+        self.buttonOutletOne.setImage(imageO, forState: .Normal)
+        self.buttonOutletTwo.setImage(imageX, forState: .Normal)
+        self.buttonOutletThree.setImage(imageO, forState: .Normal)
+        self.buttonOutletFour.setImage(imageX, forState: .Normal)
+        self.buttonOutletFive.setImage(imageX, forState: .Normal)
+        self.buttonOutletSix.setImage(imageX, forState: .Normal)
+        self.buttonOutletSeven.setImage(imageO, forState: .Normal)
+        self.buttonOutletEight.setImage(imageX, forState: .Normal)
         
     }
-   
+    
+    var imageX = UIImage(named: "x")
+    var imageO = UIImage(named: "o")
     
     @IBOutlet weak var buttonOutletZero: UIButton!
     @IBOutlet weak var buttonOutletOne: UIButton!
